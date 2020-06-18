129680720d2a6a33706bc3df7c7c2cf164078f66
Pheelix <Pheelix14@hotmail.com>
2020-06-18 15:50:29 -0700

    Update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -996,11 +996,11 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -50, -16, 2.0 }
+#define NOZZLE_TO_PROBE_OFFSET { -50, -16, -2.0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 0
+#define PROBING_MARGIN 10
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 15000
