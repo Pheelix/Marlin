1ce2494c045c1fea58646b328e95078e73893937
Pheelix <Pheelix14@hotmail.com>
2021-05-23 03:30:42 -0700

    update

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.7.2-BF - 5-23-2021"
+#define SHORT_BUILD_VERSION "2.0.7.2 - 5-23-2021"
 
 /**
  * Verbose version identifier which should contain a reference to the location
