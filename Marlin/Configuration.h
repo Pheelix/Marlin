fcb7dcfbc0d47daf3c8bedb3f531fbdb47c05e18
Pheelix <Pheelix14@hotmail.com>
2021-12-10 03:06:44 -0800

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -639,13 +639,13 @@
 
 #if ENABLED(PIDTEMPBED)
   //#define MIN_BED_POWER 0
-  //#define PID_BED_DEBUG // Sends debug data to the serial port.
+  #define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
   // Creality Stock - Ender 3
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
+  #define DEFAULT_bedKp 15.00
+  #define DEFAULT_bedKi 1.023
   #define DEFAULT_bedKd 305.4
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
--- Marlin/Version.h
+++ Marlin/Version.h
@@ -28,7 +28,7 @@
 /**
  * Marlin release version identifier
  */
-#define SHORT_BUILD_VERSION "2.0.9.2 - 11-11-2021"
+#define SHORT_BUILD_VERSION "2.0.9.2 - 12-10-2021"
 
 /**
  * Verbose version identifier which should contain a reference to the location
@@ -41,7 +41,7 @@
  * here we define this default string as the date where the latest release
  * version was tagged.
  */
-#define STRING_DISTRIBUTION_DATE "11-11-2021"
+#define STRING_DISTRIBUTION_DATE "12-10-2021"
 
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
