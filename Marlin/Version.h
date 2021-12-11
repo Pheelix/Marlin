91e22f59b31fd5e37a9183992d1f18744aecc45a
Pheelix <Pheelix14@hotmail.com>
2021-12-10 16:28:07 -0800

    Update Version.h

--- Marlin/Version.h
+++ Marlin/Version.h
@@ -46,7 +46,7 @@
 /**
  * Defines a generic printer name to be output to the LCD after booting Marlin.
  */
-#define MACHINE_NAME "Ender 3 - SKR 1.4T"
+#define MACHINE_NAME "Ender 3 - SKR 1.3"
 
 /**
  * The SOURCE_CODE_URL is the location where users will find the Marlin Source
