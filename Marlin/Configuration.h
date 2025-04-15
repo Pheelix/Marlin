4d29c8213952286327ba4aab5595b7fa8da02094
Pheelix <Pheelix14@hotmail.com>
2025-04-15 03:05:54 -0700

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1931,7 +1931,7 @@
 #define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
   #define LEVELING_NOZZLE_TEMP   0   // (°C) Only applies to E0 at this time
-  #define LEVELING_BED_TEMP     70
+  #define LEVELING_BED_TEMP     60
 #endif
 
 /**
