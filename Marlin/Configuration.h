54fa348e29e04d90441959940e043202df941a6a
Pheelix <Pheelix14@hotmail.com>
2026-03-19 18:03:59 -0700

    update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2082,7 +2082,7 @@
 #endif
 
 // Add a menu item to move between bed corners for manual bed adjustment
-#define LCD_BED_TRAMMING
+//#define LCD_BED_TRAMMING
 
 #if ENABLED(LCD_BED_TRAMMING)
   #define BED_TRAMMING_INSET_LFRB { 60, 60, 60, 60 } // (mm) Left, Front, Right, Back insets
