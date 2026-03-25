8722d3ed61cb1e396ff623190fe8919b75ad8123
Pheelix <Pheelix14@hotmail.com>
2026-03-24 23:52:55 -0700

    update 1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -21,7 +21,7 @@
  */
 #pragma once
 
-#define CONFIG_EXAMPLES_DIR "Creality/Ender-3 Max"
+//#define CONFIG_EXAMPLES_DIR "Creality/Ender-3 Max"
 
 /**
  * Configuration.h
@@ -64,7 +64,7 @@
 
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(Pheelix, Ender 3 Max)" // Who made the changes.
-#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+//#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
 
